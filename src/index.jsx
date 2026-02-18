6cd6bd9b068c5a3c8faac6ecc1f6f23e83284340
suprabho <suprabho13@gmail.com>
2026-02-18 21:14:38 +0530

    vite

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
+import App from "./App.jsx";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
